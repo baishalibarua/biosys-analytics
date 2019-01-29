b3de1b4c2455a6033d5969195090efab1ed9489e
unknown <baishalibarua@email.arizona.edu>
2019-01-28 22:05:40 -0700

    homework 2

---
+++ assignments/02-bash-scripting/head.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+args=("$@")
+declare -i x=1
+declare -i limit=3
+
+if [ "$#" -lt "1" ]; then
+        echo "Usage : ./script.sh file.txt 3"
+        exit 1
+fi
+
+if [ ! -f "${args[0]}" ]; then
+	echo "${args[0]} is not a file"
+	exit 1
+fi
+
+if [ "$#" -eq "2" ]; then
+        limit=${args[1]}
+fi
+
+while read line; do
+        echo $line
+        x=x+1
+        if [ "$x" -gt "$limit" ]; then
+                exit
+        fi
+done < ${args[0]}
